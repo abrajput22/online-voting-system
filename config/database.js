88be508c6699313575b58cd3f7146f198f72ec16
abhishek rajput <abhishekrajput20252025@gmail.com>
2025-09-19 10:22:56 +0530

    Fix database config for new MongoDB cluster and improve frontend

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const password = encodeURIComponent('ram@20042004');
-        const uri = `mongodb+srv://abhishekrajput:${password}@cluster0.ge5g18o.mongodb.net/voting_system`;
+        const uri = `mongodb+srv://abhishek_rajput:${password}@cluster0.kvalswb.mongodb.net/voting_system?retryWrites=true&w=majority&appName=Cluster0`;
         const conn = await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
